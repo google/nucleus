74acd574bd2e1e8dd801b8cc527a8e493b25a394
Genomics team in Google Brain <no-reply@google.com>
2018-05-03 09:19:19 -0700

    PiperOrigin-RevId: 195261725

---
+++ nucleus/io/text_reader.cc
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "nucleus/io/text_reader.h"
+#include "nucleus/io/hts_path.h"
+
+namespace tf = tensorflow;
+
+namespace nucleus {
+
+StatusOr<std::unique_ptr<TextReader>> TextReader::FromFile(const string& path) {
+  htsFile* fp = hts_open_x(path.c_str(), "r");
+
+  if (fp == nullptr) {
+    return tf::errors::NotFound("Could not open ", path);
+  } else {
+    auto reader = absl::WrapUnique(new TextReader(fp));
+    return std::move(reader);
+  }
+}
+
+TextReader::~TextReader() {
+  if (hts_file_) {
+    TF_CHECK_OK(Close());
+  }
+}
+
+StatusOr<string> TextReader::ReadLine() {
+  tf::Status status;
+  string line;
+  kstring_t k_line = {0, 0, nullptr};
+
+  int ret = hts_getline(hts_file_, '\n', &k_line);
+  if (ret == -1) {
+    status = tf::errors::OutOfRange("EOF");
+  } else if (ret < 0) {
+    status = tf::errors::DataLoss("Failed to read text line");
+  }
+
+  if (k_line.s) {
+    line = std::string(k_line.s);
+    free(k_line.s);
+  }
+  if (status.ok()) {
+    return line;
+  } else {
+    return status;
+  }
+}
+
+tf::Status TextReader::Close() {
+  if (!hts_file_) {
+    return tf::errors::FailedPrecondition(
+        "Cannot close an already closed file writer");
+  }
+  int hts_ok = hts_close(hts_file_);
+  hts_file_ = nullptr;
+  if (hts_ok < 0) {
+    return tf::errors::Internal("hts_close() failed with return code ", hts_ok);
+  }
+  return tf::Status::OK();
+}
+
+TextReader::TextReader(htsFile* hts_file)
+    : hts_file_(hts_file) {
+  CHECK(hts_file_ != nullptr);
+}
+
+}  // namespace nucleus
---
+++ nucleus/io/text_reader.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef THIRD_PARTY_NUCLEUS_IO_TEXT_READER_H_
+#define THIRD_PARTY_NUCLEUS_IO_TEXT_READER_H_
+
+#include "absl/memory/memory.h"
+#include "htslib/hts.h"
+#include "nucleus/vendor/statusor.h"
+#include "tensorflow/core/lib/core/status.h"
+
+namespace nucleus {
+
+using tensorflow::string;
+
+// The TextReader class allows reading text from a (possibly compressed) file.
+class TextReader {
+ public:
+  // Factory method to construct a TextReader.
+  // File compression is determined from file magic (contents), not filename.
+  static StatusOr<std::unique_ptr<TextReader>> FromFile(const string& path);
+
+  // Destructor; closes the file, if it's still open.
+  ~TextReader();
+
+  // Reads a single line from the file.
+  // Returns:
+  //  - the string line (excluding trailing newline) if read is successful;
+  //  - a status of tf::errors::OutOfRange if at end-of-file;
+  //  - otherwise, an appropriate error Status.
+  StatusOr<string> ReadLine();
+
+  // Explicitly closes the underlying file stream.
+  tensorflow::Status Close();
+
+ private:
+  // Private constructor.
+  TextReader(htsFile* hts_file);
+
+  // Underlying htslib file stream.
+  htsFile* hts_file_;
+};
+
+
+}  // namespace nucleus
+
+#endif  // THIRD_PARTY_NUCLEUS_IO_TEXT_READER_H_
