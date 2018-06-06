2e30c4a1dddd1bb4ccf65e8e2c012fd881df20ad
Genomics team in Google Brain <no-reply@google.com>
2018-06-06 11:19:43 -0700

    nucleus: Remove unused constant (nit) PiperOrigin-RevId: 199496113

--- nucleus/io/bed_reader.cc
+++ nucleus/io/bed_reader.cc
@@ -32,9 +32,6 @@ namespace nucleus {
 
 namespace tf = tensorflow;
 
-// 256 KB read buffer.
-constexpr int READER_BUFFER_SIZE = 256 * 1024;
-
 // BED-specific attributes.
 constexpr char BED_COMMENT_PREFIX[] = "#";
 
