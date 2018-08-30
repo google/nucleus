c06892bd74455c78ee3425d5121015f3d4f47399
xunjieli <xunjieli@google.com>
2018-08-30 11:04:13 -0700

    This adds a fasta reader for iterating through unindexed fasta files.
    PiperOrigin-RevId: 210942467

---
+++ nucleus/io/unindexed_fasta_reader.h
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef THIRD_PARTY_NUCLEUS_IO_UNINDEXED_FASTA_READER_H_
+#define THIRD_PARTY_NUCLEUS_IO_UNINDEXED_FASTA_READER_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "absl/types/optional.h"
+#include "htslib/faidx.h"
+#include "nucleus/io/reference.h"
+#include "nucleus/io/text_reader.h"
+#include "nucleus/platform/types.h"
+#include "nucleus/protos/range.pb.h"
+#include "nucleus/protos/reference.pb.h"
+#include "nucleus/vendor/statusor.h"
+#include "tensorflow/core/lib/core/status.h"
+
+namespace nucleus {
+
+// A FASTA reader that is not backed by a htslib FAI index.
+//
+// FASTA files store information about DNA/RNA/Amino Acid sequences:
+//
+// https://en.wikipedia.org/wiki/FASTA_format
+//
+// This reader is for FASTA files that contain many small records and are
+// explicitly not indexed. The FASTA files can be optionally block-gzipped
+// compressed.
+//
+// This class provides methods to iterate through a the FASTA records but
+// doesn't support query() for the bases spanning a specific region on the
+// genome.
+//
+// The (name, bases) tuple returned by iterate() are strings containing the
+// bases in uppercase.
+class UnindexedFastaReader : public GenomeReference {
+ public:
+  // Creates a new GenomeReference backed by the FASTA file fasta_path.
+  //
+  // Returns this newly allocated UnindexedFastaReader object, passing ownership
+  // to the caller via a unique_ptr.
+  static StatusOr<std::unique_ptr<UnindexedFastaReader>> FromFile(
+      const string& fasta_path);
+
+  ~UnindexedFastaReader();
+
+  // Disable copy and assignment operations
+  UnindexedFastaReader(const UnindexedFastaReader& other) = delete;
+  UnindexedFastaReader& operator=(const UnindexedFastaReader&) = delete;
+
+  const std::vector<nucleus::genomics::v1::ContigInfo>& Contigs()
+      const override;
+
+  StatusOr<string> GetBases(
+      const nucleus::genomics::v1::Range& range) const override;
+
+  StatusOr<std::shared_ptr<GenomeReferenceRecordIterable>> Iterate()
+      const override;
+
+  // Close the underlying resource descriptors.
+  tensorflow::Status Close() override;
+
+ private:
+  // Allow iteration to access the underlying reader.
+  friend class UnindexedFastaReaderIterable;
+
+  // Must use one of the static factory methods.
+  UnindexedFastaReader(std::unique_ptr<TextReader> text_reader);
+
+  const std::vector<nucleus::genomics::v1::ContigInfo> contigs_;
+
+  // Underlying file reader.
+  std::unique_ptr<TextReader> text_reader_;
+};
+
+}  // namespace nucleus
+
+#endif  // THIRD_PARTY_NUCLEUS_IO_UNINDEXED_FASTA_READER_H_
---
+++ nucleus/io/unindexed_fasta_reader_test.cc
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "nucleus/io/unindexed_fasta_reader.h"
+
+#include <utility>
+
+#include "tensorflow/core/platform/test.h"
+#include "nucleus/testing/test_utils.h"
+#include "nucleus/util/utils.h"
+#include "nucleus/vendor/status_matchers.h"
+#include "tensorflow/core/platform/logging.h"
+
+namespace nucleus {
+
+TEST(UnindexedFastaReaderTest, ReturnsBadStatusIfFileIsMissing) {
+  StatusOr<std::unique_ptr<UnindexedFastaReader>> result =
+      UnindexedFastaReader::FromFile(GetTestData("nonexistent.fasta"));
+  EXPECT_THAT(result, IsNotOKWithCodeAndMessage(tensorflow::error::NOT_FOUND,
+                                                "Could not open"));
+}
+
+TEST(UnindexedFastaReaderTest, IterateAfterCloseIsntOK) {
+  StatusOr<std::unique_ptr<UnindexedFastaReader>> result =
+      UnindexedFastaReader::FromFile(GetTestData("unindexed.fasta"));
+  auto reader = std::move(result.ValueOrDie());
+  auto iterator = reader->Iterate().ValueOrDie();
+  ASSERT_THAT(reader->Close(), IsOK());
+  GenomeReferenceRecord r;
+  StatusOr<bool> status = iterator->Next(&r);
+  EXPECT_THAT(iterator->Next(&r),
+              IsNotOKWithCodeAndMessage(
+                  tensorflow::error::FAILED_PRECONDITION,
+                  "Cannot iterate a closed UnindexedFastaReader"));
+}
+
+TEST(UnindexedFastaReaderTest, TestMalformed) {
+  StatusOr<std::unique_ptr<UnindexedFastaReader>> result =
+      UnindexedFastaReader::FromFile(GetTestData("malformed.fasta"));
+  auto reader = std::move(result.ValueOrDie());
+  auto iterator = reader->Iterate().ValueOrDie();
+  GenomeReferenceRecord r;
+  EXPECT_THAT(iterator->Next(&r),
+              IsNotOKWithCodeAndMessage(tensorflow::error::DATA_LOSS,
+                                        "Name not found in FASTA"));
+}
+
+class UnindexedFastaReaderFileTest : public ::testing::TestWithParam<string> {};
+
+// Test a couple of files that are formatted differently but should have the
+// same contents.
+INSTANTIATE_TEST_CASE_P(/* prefix */, UnindexedFastaReaderFileTest,
+                        ::testing::Values("unindexed.fasta", "test.fasta.gz",
+                                          "unindexed_emptylines.fasta"));
+
+TEST_P(UnindexedFastaReaderFileTest, TestIterate) {
+  LOG(INFO) << "testing file " << GetParam();
+  StatusOr<std::unique_ptr<UnindexedFastaReader>> result =
+      UnindexedFastaReader::FromFile(GetTestData(GetParam()));
+  auto reader = std::move(result.ValueOrDie());
+  auto iterator = reader->Iterate().ValueOrDie();
+  GenomeReferenceRecord r1;
+  StatusOr<bool> status = iterator->Next(&r1);
+  EXPECT_TRUE(status.ValueOrDie());
+  EXPECT_EQ("chrM", r1.first);
+  EXPECT_EQ(
+      "GATCACAGGTCTATCACCCTATTAACCACTCACGGGAGCTCTCCATGCATTTGGTATTTTCGTCTGGGGGGT"
+      "GTGCACGCGATAGCATTGCGAGACGCTG",
+      r1.second);
+
+  GenomeReferenceRecord r2;
+  status = iterator->Next(&r2);
+  EXPECT_TRUE(status.ValueOrDie());
+  EXPECT_EQ("chr1", r2.first);
+  EXPECT_EQ(
+      "ACCACCATCCTCCGTGAAATCAATATCCCGCACAAGAGTGCTACTCTCCTAAATCCCTTCTCGTCCCCATGG"
+      "ATGA",
+      r2.second);
+  GenomeReferenceRecord r3;
+  status = iterator->Next(&r3);
+  EXPECT_TRUE(status.ValueOrDie());
+  EXPECT_EQ("chr2", r3.first);
+  EXPECT_EQ(
+      "CGCTNCGGGCCCATAACACTTGGGGGTAGCTAAAGTGAACTGTATCCGACATCTGGTTCCTACTTCAGGGCC"
+      "ATAAAGCCTAAATAGCCCACACGTTCCCCTTAAATAAGACATCACGATG",
+      r3.second);
+
+  // Reading beyond the file fails.
+  GenomeReferenceRecord r4;
+  status = iterator->Next(&r4);
+  EXPECT_FALSE(status.ValueOrDie());
+}
+
+}  // namespace nucleus
