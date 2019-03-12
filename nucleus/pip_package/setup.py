c6d56bba01eb33fde760d1356f202f241b830850
Genomics team in Google Brain <no-reply@google.com>
2019-03-12 14:31:08 -0700

    Nucleus no longer depends on TensorFlow's pip package.
    PiperOrigin-RevId: 238098099

--- nucleus/pip_package/setup.py
+++ nucleus/pip_package/setup.py
@@ -90,7 +90,7 @@ TensorFlow tfrecords file may be substituted.
     # TODO(thomaswc): Consider adding some version requirements to
     # these install_requires.
     install_requires=['contextlib2', 'intervaltree', 'absl-py',
-                      'mock', 'numpy', 'six', 'tensorflow>=1.11.0'],
+                      'mock', 'numpy', 'six'],
 
     headers=headers,
 
